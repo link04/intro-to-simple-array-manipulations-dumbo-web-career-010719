1c5aa7e5f4403e383cffad2ef39be573e416e18c
Maximo Bautista <max.bautista97@gmail.com>
2018-12-24 14:43:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -21,3 +21,19 @@ end
 def using_unshift(array, string)
   array.unshift(string)
 end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
